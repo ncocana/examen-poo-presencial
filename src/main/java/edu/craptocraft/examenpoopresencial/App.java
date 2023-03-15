0b4dec9712e1f2de382f2623db5f282af1fecaee
ncocana <ncocana@cifpfbmoll.eu>
2023-03-15 15:27:19 +0100

    fix(App): redirect to interface

--- src/main/java/edu/craptocraft/examenpoopresencial/App.java
+++ src/main/java/edu/craptocraft/examenpoopresencial/App.java
@@ -3,23 +3,23 @@ package edu.craptocraft.examenpoopresencial;
 import edu.craptocraft.examenpoopresencial.barcos.BarcoCrucero;
 import edu.craptocraft.examenpoopresencial.barcos.BarcoPesquero;
 import edu.craptocraft.examenpoopresencial.barcos.BarcoPortaaviones;
+import edu.craptocraft.examenpoopresencial.barcos.Barcos;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        BarcoCrucero barcoCrucero = new BarcoCrucero(13);
-        barcoCrucero.contarCamasOcupadas("cama1", "cama2", "cama3");
+        Barcos barcoCrucero = new BarcoCrucero(13);
         System.out.println(barcoCrucero.mensajeSocorro("SOS"));
         System.out.println(barcoCrucero.toString());
 
         System.out.println("------------------------------");
-        BarcoPesquero barcoPesquero = new BarcoPesquero(13, 50, 8);
+        Barcos barcoPesquero = new BarcoPesquero(13, 50, 8);
         System.out.println(barcoPesquero.mensajeSocorro("SOS"));
         System.out.println(barcoPesquero.toString());
         
         System.out.println("------------------------------");
-        BarcoPortaaviones barcoPortaaviones = new BarcoPortaaviones(13, 30);
+        Barcos barcoPortaaviones = new BarcoPortaaviones(13, 30);
         System.out.println(barcoPortaaviones.mensajeSocorro("SOS"));
         System.out.println(barcoPortaaviones.toString());
     }
